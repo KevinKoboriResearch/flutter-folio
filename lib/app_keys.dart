799301ab636fffc0edd2aef4877eb420fefae423
Kevin Kobori <kevinyujikobori@gmail.com>
2024-06-10 02:25:23 -0300

    config: change app keys to use remottely app keys for cloudinary and firebase firestore and authentication

--- lib/app_keys.dart
+++ lib/app_keys.dart
@@ -1,6 +1,6 @@
 class AppKeys {
-  static String cloudinaryCloud = "flutterfoliodemo";
-  static String cloudinaryPreset = "ujszrrfn";
-  static String firebaseApiKey = "AIzaSyDIMnzUz9TshIyRSSl7iUpp5QxPhAcL1ZQ";
-  static String firestoreProjectId = "flutter-folio-demo";
+  static String cloudinaryCloud = "de7ijoho2";
+  static String cloudinaryPreset = "xja6qv4q";
+  static String firebaseApiKey = "AIzaSyAXR7wbxYEa8kMEV1wCMAKnVQFEdtimhzY";
+  static String firestoreProjectId = "flutter-folio-demo-2";
 }
